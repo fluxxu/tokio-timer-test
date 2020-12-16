34db78322f0472d71c1d421120b446554be42553
Flux Xu <fluxxu@gmail.com>
2020-12-15 22:59:22 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use std::time::Instant;
+use std::time::{Duration};
+use tokio::time::{delay_until, Instant};
+use tokio::runtime::Builder;
 
-#[tokio::main]
-async fn main() {
-    let t = Instant::now();
-    let delay1 = tokio::time::delay_for(std::time::Duration::from_millis(50));
-    delay1.await;
-    let delay2 = tokio::time::delay_for(std::time::Duration::from_millis(50));
-    delay2.await;
-    println!("d = {:?}", Instant::now() - t);
+fn main() {
+    unsafe {
+        winapi::um::processthreadsapi::SetPriorityClass(
+            winapi::um::processthreadsapi::GetCurrentProcess(),
+            winapi::um::winbase::REALTIME_PRIORITY_CLASS,
+        );
+    }
+
+    let mut rt = Builder::new().enable_all().build().unwrap();
+
+    rt.block_on(async {
+        let mut values = vec![];
+        for _ in 0..10 {
+            let real_now = Instant::now();
+            delay_until(real_now + Duration::from_millis(500)).await;
+            values.push((Instant::now() - real_now).as_millis());
+        }
+        dbg!(values);
+    });
 }
